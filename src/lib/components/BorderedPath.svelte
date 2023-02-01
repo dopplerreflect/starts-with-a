7d488a696fb8ccd2b7b5434940772ade378ce93d
David Rose <doppler@gmail.com>
2023-02-01 13:20:12 -0800

    add BorderedPath component

---
+++ src/lib/components/BorderedPath.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let d: string = '';
+	export let outerColor: string = 'white';
+	export let innerColor: string = 'black';
+	export let strokeWidth: number = 1;
+</script>
+
+<path {d} stroke={outerColor} stroke-width={strokeWidth} fill="none" />
+<path {d} stroke={innerColor} stroke-width={strokeWidth / 3} fill="none" />
--- src/routes/foo/+page.svelte
+++ src/routes/foo/+page.svelte
@@ -2,10 +2,19 @@
 
 <script lang="ts">
 	import { polygonPath, PHI, starPath } from '$lib/geometry';
+	import BorderedPath from '$lib/components/BorderedPath.svelte';
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="-101 -101 202 202">
 	<path d="m-101,-101H101V101H-101Z" fill="black" />
+	<circle r={100} stroke="white" fill="none" />
 	<path d={starPath(100 * (PHI - 1))} stroke="white" fill="none" />
 	<path d={starPath(100 * (PHI - 1) ** 2, { rotate: -36 })} stroke="white" fill="none" />
+	<path
+		d={starPath(100 * (PHI - 1) ** 3, { rotate: -36 })}
+		stroke="white"
+		fill="none"
+		stroke-linejoin="bevel"
+	/>
+	<BorderedPath d={starPath(100)} innerColor="hsl(255, 100%, 50%)" />
 </svg>
