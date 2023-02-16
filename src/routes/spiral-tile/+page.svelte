b9df1051b3da5163be59d5a8c3aedc12ae6789e4
David Rose <doppler@gmail.com>
2023-02-15 18:35:12 -0800

    green spiral tile

--- src/routes/spiral-tile/+page.svelte
+++ src/routes/spiral-tile/+page.svelte
@@ -14,31 +14,23 @@
 		let radius = (outerRadius / count) * k;
 		return radialPoint(angle, radius);
 	});
-	let pointStrings = points.map((p) => `${p.x},${p.y}`);
-	let cellPaths = [...Array(count).keys()].map((k, i) => {
-		if (pointStrings[k + 21])
-			return `M${i < 13 ? '0,0' : pointStrings[k]}L${pointStrings[k + 13]}L${
-				pointStrings[k + 21]
-			}L${pointStrings[k + 8]}Z`;
-	});
-	let scaledCells = [...Array(count).keys()].map((k, i) => {
-		if (points[k + 21]) {
-			let polygon = [
-				i < 13 ? [0, 0] : [points[k].x, points[k].y],
-				[points[k + 13].x, points[k + 13].y],
-				[points[k + 21].x, points[k + 21].y],
-				[points[k + 8].x, points[k + 8].y]
-			];
-			return polygonScale(polygon, PHI - 1);
-		}
-	});
-	let scaledCellPaths = scaledCells
+	let polygons = [...Array(count).keys()]
 		.map(
-			(c) =>
-				c &&
-				`M${c[0][0]},${c[0][1]}L${c[1][0]},${c[1][1]}L${c[2][0]},${c[2][1]}L${c[3][0]},${c[3][1]}Z`
+			(k) =>
+				points[k + 21] && [
+					k < 13 ? [0, 0] : [points[k].x, points[k].y],
+					[points[k + 13].x, points[k + 13].y],
+					[points[k + 21].x, points[k + 21].y],
+					[points[k + 8].x, points[k + 8].y]
+				]
 		)
 		.filter((e) => e !== undefined);
+	let scaledPolygons = (scale: number) => polygons.map((p) => polygonScale(p, scale));
+	let scaledPolygonPaths = (scale: number) =>
+		scaledPolygons(scale).map(
+			(c) =>
+				`M${c[0][0]},${c[0][1]}L${c[1][0]},${c[1][1]}L${c[2][0]},${c[2][1]}L${c[3][0]},${c[3][1]}Z`
+		);
 	let svg: SVGSVGElement;
 	onMount(() => useSaveFile(svg));
 </script>
@@ -51,18 +43,18 @@
 >
 	<defs>
 		<radialGradient id="rGradient">
-			<stop offset="80%" stop-color="hsl(240, 100%, 50%)" />
-			<stop offset="100%" stop-color="hsl(210, 100%, 50%)" />
+			<stop offset="0%" stop-color="hsl(90, 100%, 50%)" />
+			<stop offset="100%" stop-color="hsl(120, 100%, 15%)" />
 		</radialGradient>
 		<filter id="blur">
-			<feOffset result="offOut" in="SourceGraphic" dx="0" dy={size / 360} />
+			<feOffset result="offOut" in="SourceGraphic" dx="0" dy={size / 270} />
 			<feColorMatrix
 				result="matrixOut"
 				in="offOut"
 				type="matrix"
-				values="0.1 0 0 0 0 0 0.1 0 0 0 0 0 0.1 0 0 0 0 0 1 0"
+				values="0.25 0 0 0 0 0 0.25 0 0 0 0 0 0.25 0 0 0 0 0 1 0"
 			/>
-			<feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="20" />
+			<feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="10" />
 			<feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
 		</filter>
 	</defs>
@@ -70,34 +62,37 @@
 		d={`M${-size / 2},${-size / 2}H${size / 2}V${size / 2}H${-size / 2}Z`}
 		fill="url(#rGradient)"
 	/>
-	<g id="figure" filter="url(#blur)">
-		{#each cellPaths as path, i}
+	<g id="level1" filter="url(#blur)">
+		{#each scaledPolygonPaths(1) as path, i}
 			<path
 				d={path}
-				fill={`hsla(${(i % 21) + 30}, 100%, 50%, 1)`}
-				stroke={`hsla(${(i % 21) + 0}, 100%, 20%, 1)`}
+				fill={`hsla(90,100%,50%,0.2)`}
+				stroke={`hsla(120, 100%, 50%, 0.25)`}
 				stroke-width={size / 360}
+				stroke-linejoin="round"
 			/>
 		{/each}
 	</g>
-	<g id="figure" filter="url(#blur)">
-		{#each scaledCellPaths as path, i}
+	<g id="level2" filter="url(#blur)">
+		{#each scaledPolygonPaths(PHI - 1) as path, i}
 			<path
 				d={path}
-				fill={`hsla(${(i % 21) + 210}, 100%, 50%, 1)`}
-				stroke={`hsla(${(i % 21) + 240}, 100%, 25%, 1)`}
-				stroke-width={size / 720}
+				fill={`hsla(${(i % 21) + 90}, 100%, 50%, 1)`}
+				stroke={`hsla(${(i % 21) + 120}, 100%, 50%, 1)`}
+				stroke-width={size / 360}
+				stroke-linejoin="round"
 			/>
 		{/each}
 	</g>
-	<!-- {#each points as p, i}
-		<text
-			x={p.x}
-			y={p.y}
-			alignment-baseline="middle"
-			text-anchor="middle"
-			fill="black"
-			font-size={16 / zoom}>{i}</text
-		>
-	{/each} -->
+	<g id="level1" filter="url(#blur)">
+		{#each scaledPolygonPaths((PHI - 1) ** 2) as path, i}
+			<path
+				d={path}
+				fill={`hsla(${(i % 21) + 120}, 100%, 50%, 1)`}
+				stroke={`hsla(${(i % 21) + 150}, 100%, 50%, 1)`}
+				stroke-width={size / 720}
+				stroke-linejoin="round"
+			/>
+		{/each}
+	</g>
 </svg>
