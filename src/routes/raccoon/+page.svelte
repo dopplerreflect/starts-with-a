3d9238b02d165c516c466fc2e15325fb1e266414
David Rose <doppler@gmail.com>
2023-03-11 21:47:19 -0800

    spirally, colorful raccoon thing

--- src/routes/raccoon/+page.svelte
+++ src/routes/raccoon/+page.svelte
@@ -2,12 +2,11 @@
 	import { φ, radialPoint, radialPointString } from '$lib/geometry';
 	import { useSaveFile } from '$lib/save-svg';
 	import { onMount } from 'svelte';
-	const size = 512;
-	const blueprintColor = 'hsl(220, 33%, 66%)';
-	const angles = [...Array(36).keys()].map((k) => 10 * k);
-	const radii = [...Array(5).keys()].map((k) => (size / 2) * φ ** k);
-	const eyeRadius = (radii[2] - radii[3]) / 2;
-	const noseRadius = (radii[1] - radii[2]) / 2;
+	import RaccoonFace from './raccoonFace.svelte';
+	const size = 4096;
+	let count = 360;
+	const angles = [...Array(count).keys()].map((k) => k * (360 * φ));
+	console.log(angles);
 	let svg: SVGSVGElement;
 	onMount(() => useSaveFile(svg));
 </script>
@@ -18,207 +17,17 @@
 	xmlns="http://www.w3.org/2000/svg"
 	viewBox={`${-size / 2} ${-size / 2} ${size} ${size}`}
 >
-	<defs>
-		<g id="guide">
-			{#each radii as r}
-				<circle {r} stroke={blueprintColor} fill="none" />
-			{/each}
-			{#each angles as a, i}
-				<path
-					d={`M${radialPointString(a, radii[4])}L${radialPointString(a, radii[0])}`}
-					stroke={blueprintColor}
-				/>
-				<text
-					x={radialPoint(a, radii[1]).x}
-					y={radialPoint(a, radii[1]).y}
-					fill={blueprintColor}
-					alignment-baseline="middle"
-					text-anchor="middle">{i}</text
-				>
-			{/each}
-		</g>
-	</defs>
 	<path
 		d={`M${-size / 2},${-size / 2}H${size / 2}V${size / 2}H${-size / 2}Z`}
-		fill="hsl(0, 0%, 75%)"
+		fill="hsl(0, 0%, 5%)"
 	/>
-	<!-- <image x={-size / 2} y={-size / 2} width={size} height={size} href="/raccoon.jpg" /> -->
-	<!-- <use href="#guide" /> -->
-	<g id="raccoonFace">
-		<g id="leftEar">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[18], radii[1])],
-					['C'],
-					[radialPointString(angles[20], radii[0])],
-					[radialPointString(angles[21], radii[0])],
-					[radialPointString(angles[22], radii[0])],
-					['S'],
-					[radialPointString(angles[23], radii[0])],
-					[radialPointString(angles[24], radii[1])]
-				].join(' ')}
-				stroke="black"
-				fill="hsla(0, 0%, 100%, 1)"
-			/>
-		</g>
-		<g id="rightEar">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[0], radii[1])],
-					['C'],
-					[radialPointString(angles[34], radii[0])],
-					[radialPointString(angles[33], radii[0])],
-					[radialPointString(angles[32], radii[0])],
-					['S'],
-					[radialPointString(angles[31], radii[0])],
-					[radialPointString(angles[30], radii[1])]
-				].join(' ')}
-				stroke="black"
-				fill="hsla(0, 0%, 100%, 1)"
-			/>
-		</g>
-		<g id="skull">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[30], radii[1])],
-					['A'],
-					[radii[1]],
-					[radii[1]],
-					['0 0 0'],
-					[radialPointString(angles[24], radii[1])],
-					['A'],
-					[radii[0]],
-					[radii[0]],
-					['0 0 0'],
-					[radialPointString(angles[14], radii[0])],
-					['A'],
-					[radii[1]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[12], radii[1])],
-					['A'],
-					[radii[2]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[6], radii[1])],
-					['A'],
-					[radii[1]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[4], radii[0])],
-					['A'],
-					[radii[0]],
-					[radii[0]],
-					['0 0 0'],
-					[radialPointString(angles[30], radii[1])],
-					['Z']
-				].join(' ')}
-				stroke="black"
-				fill="hsla(0, 0%, 50%, 1)"
-			/>
-		</g>
-		<g id="leftCheek">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[14], radii[0])],
-					['C'],
-					[radialPointString(angles[16], radii[0])],
-					[radialPointString(angles[18], radii[2])],
-					[radialPointString(angles[17], radii[3])],
-					['S'],
-					[radialPointString(angles[15], radii[4])],
-					[radialPointString(angles[12], radii[1])],
-					['A'],
-					[radii[1]],
-					[radii[1]],
-					['0 0 0'],
-					[radialPointString(angles[14], radii[0])]
-				].join(' ')}
-				stroke="black"
-				fill="hsla(0, 0%, 25%, 1)"
-			/>
-		</g>
-		<g id="leftEye">
-			<circle
-				cx={radialPoint(angles[15], radii[3] + eyeRadius).x}
-				cy={radialPoint(angles[15], radii[3] + eyeRadius).y}
-				r={eyeRadius}
-				fill="hsl(0, 0%, 0%, 1)"
-			/>
-			<circle
-				cx={radialPoint(angles[15], radii[3] + eyeRadius * φ).x}
-				cy={radialPoint(angles[15], radii[3] + eyeRadius * φ).y}
-				r={eyeRadius * φ ** 2}
-				fill="white"
-			/>
-		</g>
-		<g id="rightCheek">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[4], radii[0])],
-					['C'],
-					[radialPointString(angles[2], radii[0])],
-					[radialPointString(angles[0], radii[2])],
-					[radialPointString(angles[1], radii[3])],
-					['S'],
-					[radialPointString(angles[3], radii[4])],
-					[radialPointString(angles[6], radii[1])],
-					['A'],
-					[radii[1]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[4], radii[0])]
-				].join(' ')}
-				stroke="black"
-				fill="hsla(0, 0%, 25%, 1)"
-			/>
-		</g>
-		<g id="rightEye">
-			<circle
-				cx={radialPoint(angles[3], radii[3] + eyeRadius).x}
-				cy={radialPoint(angles[3], radii[3] + eyeRadius).y}
-				r={eyeRadius}
-				fill="hsl(0, 0%, 0%, 1)"
-			/>
-			<circle
-				cx={radialPoint(angles[3], radii[3] + eyeRadius * φ).x}
-				cy={radialPoint(angles[3], radii[3] + eyeRadius * φ).y}
-				r={eyeRadius * φ ** 2}
-				fill="white"
-			/>
-		</g>
-		<g id="snout">
-			<path
-				d={[
-					['M'],
-					[radialPointString(angles[12], radii[1])],
-					['A'],
-					[radii[2]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[6], radii[1])],
-					['A'],
-					[radii[2]],
-					[radii[1]],
-					['0 0 1'],
-					[radialPointString(angles[12], radii[1])]
-				].join(' ')}
-				stroke="black"
-				fill="hsl(0, 0%, 100%, 1)"
-			/>
-		</g>
-		<g id="nose">
-			<circle
-				cx={radialPoint(angles[9], radii[2] + noseRadius).x}
-				cy={radialPoint(angles[9], radii[2] + noseRadius).y}
-				r={noseRadius}
-				fill="hsl(0, 0%, 0%, 1)"
-			/>
-		</g>
-	</g>
+	<!-- <RaccoonFace size={512} rotate={90} center={{ x: 100, y: 100 }} /> -->
+	{#each angles as a, i}
+		<RaccoonFace
+			size={i < 15 ? 100 : 100 + i * 1.25}
+			rotate={a + 90}
+			center={radialPoint(a, (size / count) * i)}
+			hue={180 + i / 1.75}
+		/>
+	{/each}
 </svg>
---
+++ src/routes/raccoon/raccoonFace.svelte
@@ -0,0 +1,195 @@
+<script lang="ts">
+	import { φ, radialPoint, radialPointString } from '$lib/geometry';
+	export let size = 512;
+	export let rotate = 0;
+	export let center = { x: 0, y: 0 };
+	export let hue = 0;
+	const angles = [...Array(36).keys()].map((k) => 10 * k);
+	const radii = [...Array(5).keys()].map((k) => (size / 2) * φ ** k);
+	const eyeRadius = (radii[2] - radii[3]) / 2;
+	const noseRadius = (radii[1] - radii[2]) / 2;
+</script>
+
+<g id="raccoonFace" transform={`rotate(${rotate}, ${center.x}, ${center.y})`}>
+	<g id="leftEar">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[18], radii[1], { center })],
+				['C'],
+				[radialPointString(angles[20], radii[0], { center })],
+				[radialPointString(angles[21], radii[0], { center })],
+				[radialPointString(angles[22], radii[0], { center })],
+				['S'],
+				[radialPointString(angles[23], radii[0], { center })],
+				[radialPointString(angles[24], radii[1], { center })]
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 90%)`}
+		/>
+	</g>
+	<g id="rightEar">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[0], radii[1], { center })],
+				['C'],
+				[radialPointString(angles[34], radii[0], { center })],
+				[radialPointString(angles[33], radii[0], { center })],
+				[radialPointString(angles[32], radii[0], { center })],
+				['S'],
+				[radialPointString(angles[31], radii[0], { center })],
+				[radialPointString(angles[30], radii[1], { center })]
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 90%)`}
+		/>
+	</g>
+	<g id="skull">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[30], radii[1], { center })],
+				['A'],
+				[radii[1]],
+				[radii[1]],
+				['0 0 0'],
+				[radialPointString(angles[24], radii[1], { center })],
+				['A'],
+				[radii[0]],
+				[radii[0]],
+				['0 0 0'],
+				[radialPointString(angles[14], radii[0], { center })],
+				['A'],
+				[radii[1]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[12], radii[1], { center })],
+				['A'],
+				[radii[2]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[6], radii[1], { center })],
+				['A'],
+				[radii[1]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[4], radii[0], { center })],
+				['A'],
+				[radii[0]],
+				[radii[0]],
+				['0 0 0'],
+				[radialPointString(angles[30], radii[1], { center })],
+				['Z']
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 50%)`}
+		/>
+	</g>
+	<g id="leftCheek">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[14], radii[0], { center })],
+				['C'],
+				[radialPointString(angles[16], radii[0], { center })],
+				[radialPointString(angles[18], radii[2], { center })],
+				[radialPointString(angles[17], radii[3], { center })],
+				['S'],
+				[radialPointString(angles[15], radii[4], { center })],
+				[radialPointString(angles[12], radii[1], { center })],
+				['A'],
+				[radii[1]],
+				[radii[1]],
+				['0 0 0'],
+				[radialPointString(angles[14], radii[0], { center })]
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 25%)`}
+		/>
+	</g>
+	<g id="leftEye">
+		<circle
+			cx={radialPoint(angles[15], radii[3] + eyeRadius, { center }).x}
+			cy={radialPoint(angles[15], radii[3] + eyeRadius, { center }).y}
+			r={eyeRadius}
+			fill="hsl(0, 0%, 0%)"
+		/>
+		<circle
+			cx={radialPoint(angles[15], radii[3] + eyeRadius * φ, { center }).x}
+			cy={radialPoint(angles[15], radii[3] + eyeRadius * φ, { center }).y}
+			r={eyeRadius * φ ** 2}
+			fill="white"
+		/>
+	</g>
+	<g id="rightCheek">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[4], radii[0], { center })],
+				['C'],
+				[radialPointString(angles[2], radii[0], { center })],
+				[radialPointString(angles[0], radii[2], { center })],
+				[radialPointString(angles[1], radii[3], { center })],
+				['S'],
+				[radialPointString(angles[3], radii[4], { center })],
+				[radialPointString(angles[6], radii[1], { center })],
+				['A'],
+				[radii[1]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[4], radii[0], { center })]
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 25%)`}
+		/>
+	</g>
+	<g id="rightEye">
+		<circle
+			cx={radialPoint(angles[3], radii[3] + eyeRadius, { center }).x}
+			cy={radialPoint(angles[3], radii[3] + eyeRadius, { center }).y}
+			r={eyeRadius}
+			fill="hsl(0, 0%, 0%, 1)"
+		/>
+		<circle
+			cx={radialPoint(angles[3], radii[3] + eyeRadius * φ, { center }).x}
+			cy={radialPoint(angles[3], radii[3] + eyeRadius * φ, { center }).y}
+			r={eyeRadius * φ ** 2}
+			fill="white"
+		/>
+	</g>
+	<g id="snout">
+		<path
+			d={[
+				['M'],
+				[radialPointString(angles[12], radii[1], { center })],
+				['A'],
+				[radii[2]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[6], radii[1], { center })],
+				['A'],
+				[radii[2]],
+				[radii[1]],
+				['0 0 1'],
+				[radialPointString(angles[12], radii[1], { center })]
+			].join(' ')}
+			stroke="black"
+			stroke-width={size / 128}
+			fill={`hsl(${hue}, 100%, 90%)`}
+		/>
+	</g>
+	<g id="nose">
+		<circle
+			cx={radialPoint(angles[9], radii[2] + noseRadius, { center }).x}
+			cy={radialPoint(angles[9], radii[2] + noseRadius, { center }).y}
+			r={noseRadius}
+			fill="hsl(0, 0%, 0%, 1)"
+		/>
+	</g>
+</g>
