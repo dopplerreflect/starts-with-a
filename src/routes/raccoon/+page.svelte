0bea956f256a5e47342ef053606a7fb239f8904b
David Rose <doppler@gmail.com>
2023-03-12 13:48:51 -0700

    tweeks to raccoon spiral

--- src/routes/raccoon/+page.svelte
+++ src/routes/raccoon/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import { φ, radialPoint, radialPointString } from '$lib/geometry';
+	import { Φ, φ, radialPoint } from '$lib/geometry';
 	import { useSaveFile } from '$lib/save-svg';
 	import { onMount } from 'svelte';
 	import RaccoonFace from './raccoonFace.svelte';
-	const size = 4096;
-	let count = 360;
+	const size = 8092;
+	const count = 360;
 	const angles = [...Array(count).keys()].map((k) => k * (360 * φ));
-	console.log(angles);
+	const hues = { start: 330, end: 170 };
+	function hue(i: number) {
+		return hues.start - Math.abs(((hues.start - hues.end) / count) * i);
+	}
 	let svg: SVGSVGElement;
 	onMount(() => useSaveFile(svg));
 </script>
@@ -19,15 +22,15 @@
 >
 	<path
 		d={`M${-size / 2},${-size / 2}H${size / 2}V${size / 2}H${-size / 2}Z`}
-		fill="hsl(0, 0%, 5%)"
+		fill="hsl(0, 0%, 100%)"
 	/>
-	<!-- <RaccoonFace size={512} rotate={90} center={{ x: 100, y: 100 }} /> -->
 	{#each angles as a, i}
 		<RaccoonFace
-			size={i < 15 ? 100 : 100 + i * 1.25}
+			size={i < 15 ? size / 80.92 : size / 80.92 + i * (size / Φ ** 17.3)}
 			rotate={a + 90}
-			center={radialPoint(a, (size / count) * i)}
-			hue={180 + i / 1.75}
+			center={radialPoint(a, (size / 1.4 / count) * i)}
+			hue={hue(i)}
 		/>
 	{/each}
+	<RaccoonFace size={size / 4} rotate={0} center={{ x: 0, y: 0 }} hue={330} />
 </svg>
