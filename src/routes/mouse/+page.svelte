419c9e446f5699550db2d2ab0e1ed19bbe12b937
David Rose <doppler@gmail.com>
2023-03-24 15:01:46 -0700

    mouse: highlight closest angle and radius

--- src/routes/mouse/+page.svelte
+++ src/routes/mouse/+page.svelte
@@ -7,11 +7,13 @@
 	let size = browser ? Math.min(window.innerHeight, window.innerWidth) : 2 ** 8;
 	let r = 0;
 	let a = 0;
+	let closestAngle = 0;
+	let closestRadius = 0;
 
 	let fill = 'hsl(220, 50%, 50%)';
 
-	const angles = arrayMap(8, (n) => (360 / 8) * n);
-	const radii = arrayMap(8, (n) => Math.round((size / 2) * phi * phi ** n));
+	const angles = arrayMap(48, (n) => (360 / 48) * n);
+	const radii = arrayMap(8, (n) => Math.round((size / 2) * phi ** n));
 
 	const intersections = angles
 		.map((a, ai) => [...radii.map((r, ri) => radialPoint(angles[ai], radii[ri]))])
@@ -21,8 +23,14 @@
 	function handleMouseMove(event: MouseEvent) {
 		mouse = { x: -size / 2 + event.offsetX, y: -size / 2 + event.offsetY };
 		r = Math.sqrt(mouse.x ** 2 + mouse.y ** 2);
+		closestRadius = radii.reduce((prev, curr) =>
+			Math.abs(curr - r) < Math.abs(prev - r) ? curr : prev
+		);
 		a = -(Math.atan2(mouse.x, mouse.y) * 180) / Math.PI + 90;
 		if (mouse.x > 0 && mouse.y < 0) a = 270 + 90 + a;
+		closestAngle = angles.reduce((prev, curr) =>
+			Math.abs(curr - a) < Math.abs(prev - a) ? curr : prev
+		);
 	}
 
 	onMount(() => {
@@ -44,11 +52,11 @@
 			d={`M${radialPointString(a, radii[0])}L${radialPointString(a, radii[radii.length - 1])}`}
 			stroke="white"
 		/>
-		<text x={radialPoint(a, radii[0]).x} y={radialPoint(a, radii[0]).y} fill="white" font-size="2em"
+		<!-- <text x={radialPoint(a, radii[0]).x} y={radialPoint(a, radii[0]).y} fill="white" font-size="2em"
 			>{a}</text
-		>
+		> -->
 	{/each}
-	{#each intersections as intersection, i}
+	<!-- {#each intersections as intersection, i}
 		<text
 			x={intersection.x}
 			y={intersection.y}
@@ -57,11 +65,13 @@
 			text-anchor="middle"
 			alignment-baseline="middle">{i}</text
 		>
-	{/each}
+	{/each} -->
 	{#if mouse}
 		<circle cx={mouse.x} cy={mouse.y} r={10} stroke="white" fill="none" />
 		<circle {r} stroke="yellow" fill="none" />
 		<path d={`M0 0L${radialPointString(a, r)}`} stroke="yellow" />
-		<text x={mouse.x} y={mouse.y} font-size="3em" fill="yellow">{Math.round(a)}</text>
+		<!-- <text x={mouse.x} y={mouse.y} font-size="3em" fill="yellow">{Math.round(a)}</text> -->
 	{/if}
+	<circle r={closestRadius} stroke="red" fill="none" />
+	<path d={`M0 0L${radialPointString(closestAngle, radii[0])}`} stroke="red" />
 </svg>
