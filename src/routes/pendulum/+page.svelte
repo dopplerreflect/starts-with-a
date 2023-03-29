e416c142164e641fd74479e61f382d5ec8e8770f
David Rose <doppler@gmail.com>
2023-03-28 17:19:53 -0700

    pendulum: something?

--- src/routes/pendulum/+page.svelte
+++ src/routes/pendulum/+page.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
-	import { viewBox } from '$lib/geometry';
+	import { phi, radialPointString, viewBox } from '$lib/geometry';
 	import { onMount } from 'svelte';
 
 	let svg: SVGSVGElement;
 	const size = 2 ** 10;
 	let r = (size / 2) * 0.95;
-	let x = 0;
-	let y = 0;
+	let d = 0;
+	$: x = r * Math.cos(d * (Math.PI * 2));
+	$: y = r * Math.sin(d * (Math.PI * 2)) * phi ** 2;
+	// let y = r * phi ** 2;
+	let a = 0;
 	$: pendulum = { x, y };
-
+	let points: Point[] = [];
+	let path: string;
 	onMount(() => {
 		let timeout: number;
 		function step() {
-			console.log(new Date().getMilliseconds());
+			points = [
+				...points,
+				{
+					x:
+						pendulum.x * Math.cos(a * (Math.PI / 180)) + pendulum.y * Math.sin(a * (Math.PI / 180)),
+					y:
+						-pendulum.x * Math.sin(a * (Math.PI / 180)) + pendulum.y * Math.cos(a * (Math.PI / 180))
+				}
+			];
+			path = `M${[...points.map((p) => `${p.x} ${p.y}`)].join('L')}`;
+			console.log(path);
 			timeout = setTimeout(() => {
-				step();
-			}, 1000 / 60);
+				d = d > 1 ? 0 : d + 1 / 24;
+				a = a > 360 ? 0 : a + 1 / (r / 24);
+				r = r - size / (360 * 24);
+				if (a <= 360) step();
+			}, 0);
 		}
+		step();
 		const frame = window.requestAnimationFrame(step);
 		return () => {
 			window.cancelAnimationFrame(frame);
@@ -27,8 +45,14 @@
 </script>
 
 <svg bind:this={svg} id="pendulum" viewBox={viewBox(size)}>
-	<Background {size} fill="hsl(45, 25%, 75%)" />
-	<circle {r} stroke="black" fill="none" />
-	<path id="pendulumVerticalAxis" d={`M${pendulum.x} ${-size}V${size}`} stroke="black" />
-	<path id="pendulumHorizontalAxis" d={`M${-size} ${pendulum.y}H${size}`} stroke="black" />
+	<defs>
+		<radialGradient id="gradient">
+			<stop offset="0%" stop-color="red" />
+			<stop offset="90%" stop-color="yellow" />
+		</radialGradient>
+	</defs>
+	<Background {size} fill="hsl(220, 25%, 20%)" />
+	<g id="figure" transform={`rotate(${a})`}>
+		<path d={path} stroke="url(#gradient)" fill="none" fill-rule="evenodd" />
+	</g>
 </svg>
