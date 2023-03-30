e3d10e12180aa7c76305a1b1f954aa5143bd2e84
David Rose <doppler@gmail.com>
2023-03-30 14:01:16 -0700

    carbon-fiber: looking decent

--- src/routes/carbon-fiber/+page.svelte
+++ src/routes/carbon-fiber/+page.svelte
@@ -11,26 +11,42 @@
 
 <svg viewBox={viewBox(size)}>
 	<defs>
-		<filter id="yturbulence">
-			<feTurbulence type="turbulence" baseFrequency="0.25 0.0001" />
-			<feComposite in="SourceGraphic" in2="turbulence" operator="xor" />
-		</filter>
-		<filter id="xturbulence">
-			<feTurbulence type="turbulence" baseFrequency="0.0001 0.25" />
-			<feComposite in="SourceGraphic" in2="turbulence" operator="xor" />
-		</filter>
-		<g id="hv" stroke="hsl(240, 50%, 0%)" stroke-width="5">
-			<path
-				id="vertical"
-				d={`M0 0H${size / 4}V${size / 2}H0Z`}
-				filter="url(#yturbulence)"
-				fill="hsl(0, 0%, 5%)"
+		<filter id="filter">
+			<feDiffuseLighting in="SourceGraphic" result="light" lighting-color="hsl(220, 100%, 85%)">
+				<fePointLight x={0} y={0} z={2 ** 10} />
+			</feDiffuseLighting>
+
+			<feComposite
+				in="SourceGraphic"
+				in2="light"
+				operator="arithmetic"
+				k1="1"
+				k2="0.25"
+				k3="0"
+				k4="0"
 			/>
+		</filter>
+
+		<linearGradient id="xgradient">
+			<stop offset="0%" stop-color="black" />
+			<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
+			<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
+			<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
+			<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
+		</linearGradient>
+		<linearGradient id="ygradient" gradientTransform="rotate(90)">
+			<stop offset="0%" stop-color="black" />
+			<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
+			<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
+			<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
+			<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
+		</linearGradient>
+		<g id="hv">
+			<path id="vertical" d={`M0 0H${size / 4}V${size / 2}H0Z`} fill="url(#ygradient)" />
 			<path
 				id="horizontal"
 				d={`M0 ${size / 2}H${size / 2}V${size * 0.75}H0Z`}
-				filter="url(#xturbulence)"
-				fill="hsl(0, 0%, 0%)"
+				fill="url(#xgradient)"
 			/>
 		</g>
 		<g id="tile" transform="">
@@ -42,10 +58,9 @@
 			<use xlink:href="#hv" transform={`translate(${size / 2} ${size / 2})`} />
 			<use xlink:href="#hv" transform={`translate(${size * 0.75} ${size / 4})`} />
 		</g>
-		<pattern id="tilepattern" patternUnits="objectBoundingBox" width="0.03" height="0.03">
-			<use xlink:href="#tile" transform="scale(0.03)" />
+		<pattern id="tilepattern" patternUnits="objectBoundingBox" width="0.05" height="0.05">
+			<use xlink:href="#tile" transform="scale(0.05)" />
 		</pattern>
 	</defs>
-	<Background {size} fill={`hsl(220, 10%, 90%)`} />
-	<Background {size} fill="url(#tilepattern)" />
+	<Background {size} fill="url(#tilepattern)" filter="url(#filter)" />
 </svg>
