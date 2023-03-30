af6a7f180570a63cd21c452614a1696b2f55a172
David Rose <doppler@gmail.com>
2023-03-30 15:14:11 -0700

    extract CarbonFiberPattern

---
+++ src/lib/components/CarbonFiberPattern.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let size = 2 ** 8;
+	export let scale = 0.05;
+</script>
+
+<pattern id="CarbonFiberPattern" patternUnits="objectBoundingBox" width={scale} height={scale}>
+	<linearGradient id="carbon-fiber-xgradient">
+		<stop offset="0%" stop-color="black" />
+		<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
+		<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
+		<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
+		<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
+	</linearGradient>
+	<linearGradient id="carbon-fiber-ygradient" gradientTransform="rotate(90)">
+		<stop offset="0%" stop-color="black" />
+		<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
+		<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
+		<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
+		<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
+	</linearGradient>
+	<g id="carbon-fiber-L">
+		<path d={`M0 0H${size / 4}V${size / 2}H0Z`} fill="url(#carbon-fiber-ygradient)" />
+		<path d={`M0 ${size / 2}H${size / 2}V${size * 0.75}H0Z`} fill="url(#carbon-fiber-xgradient)" />
+	</g>
+	<g id="carbon-fiber-tile" transform="">
+		<use xlink:href="#carbon-fiber-L" />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${size / 4} ${-size / 4})`} />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${size / 2} ${-size / 2})`} />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${-size / 4} ${size / 4})`} />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${size / 4} ${size * 0.75})`} />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${size / 2} ${size / 2})`} />
+		<use xlink:href="#carbon-fiber-L" transform={`translate(${size * 0.75} ${size / 4})`} />
+	</g>
+	<use xlink:href="#carbon-fiber-tile" transform={`scale(${scale})`} />
+</pattern>
--- src/routes/carbon-fiber/+page.svelte
+++ src/routes/carbon-fiber/+page.svelte
@@ -1,66 +1,16 @@
 <svelte:options namespace="svg" />
 
 <script lang="ts">
-	import { arrayMap, viewBox } from '$lib/geometry';
+	import { viewBox } from '$lib/geometry';
 	import Background from '$lib/components/Background.svelte';
+	import CarbonFiberPattern from '$lib/components/CarbonFiberPattern.svelte';
 
 	const size = 2 ** 13;
-
-	const divs = arrayMap(4, (n) => (size / 4) * n);
 </script>
 
 <svg viewBox={viewBox(size)}>
 	<defs>
-		<filter id="filter">
-			<feDiffuseLighting in="SourceGraphic" result="light" lighting-color="hsl(220, 100%, 85%)">
-				<fePointLight x={0} y={0} z={2 ** 10} />
-			</feDiffuseLighting>
-
-			<feComposite
-				in="SourceGraphic"
-				in2="light"
-				operator="arithmetic"
-				k1="1"
-				k2="0.25"
-				k3="0"
-				k4="0"
-			/>
-		</filter>
-
-		<linearGradient id="xgradient">
-			<stop offset="0%" stop-color="black" />
-			<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
-			<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
-			<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
-			<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
-		</linearGradient>
-		<linearGradient id="ygradient" gradientTransform="rotate(90)">
-			<stop offset="0%" stop-color="black" />
-			<stop offset="25%" stop-color="hsl(0, 0%, 20%)" />
-			<stop offset="50%" stop-color="hsl(0, 0%, 30%)" />
-			<stop offset="75%" stop-color="hsl(0, 0%, 20%)" />
-			<stop offset="100%" stop-color="hsl(0, 0%, 15%)" />
-		</linearGradient>
-		<g id="hv">
-			<path id="vertical" d={`M0 0H${size / 4}V${size / 2}H0Z`} fill="url(#ygradient)" />
-			<path
-				id="horizontal"
-				d={`M0 ${size / 2}H${size / 2}V${size * 0.75}H0Z`}
-				fill="url(#xgradient)"
-			/>
-		</g>
-		<g id="tile" transform="">
-			<use xlink:href="#hv" />
-			<use xlink:href="#hv" transform={`translate(${size / 4} ${-size / 4})`} />
-			<use xlink:href="#hv" transform={`translate(${size / 2} ${-size / 2})`} />
-			<use xlink:href="#hv" transform={`translate(${-size / 4} ${size / 4})`} />
-			<use xlink:href="#hv" transform={`translate(${size / 4} ${size * 0.75})`} />
-			<use xlink:href="#hv" transform={`translate(${size / 2} ${size / 2})`} />
-			<use xlink:href="#hv" transform={`translate(${size * 0.75} ${size / 4})`} />
-		</g>
-		<pattern id="tilepattern" patternUnits="objectBoundingBox" width="0.05" height="0.05">
-			<use xlink:href="#tile" transform="scale(0.05)" />
-		</pattern>
+		<CarbonFiberPattern {size} scale={0.075} />
 	</defs>
-	<Background {size} fill="url(#tilepattern)" filter="url(#filter)" />
+	<Background {size} fill="url(#CarbonFiberPattern)" />
 </svg>
