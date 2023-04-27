01a3d3aa17bf7c1cb360ffbeb694d25e23bafa19
David Rose <doppler@gmail.com>
2023-04-26 19:13:21 -0700

    make corrections and add ray bg

--- src/routes/stellated-dodecahedron/+page.svelte
+++ src/routes/stellated-dodecahedron/+page.svelte
@@ -4,10 +4,10 @@
 	import Background from '$lib/components/Background.svelte';
 	import {
 		anglesArray,
-		arrayMap,
 		circleIntersections,
 		circleLineIntersections,
 		intersection,
+		phylotaxicPoints,
 		polygonPath,
 		radialPoint,
 		radialPointString,
@@ -50,7 +50,6 @@
 			...angles6
 				.map((a) => [
 					{ r: setupRadii[1], ...radialPoint(a, setupRadii[2]) },
-					// { r: radii[2], ...radialPoint(a, radii[2]) },
 					{ r: setupRadii[3], ...radialPoint(a, setupRadii[2]) },
 					{ r: setupRadii[4], ...radialPoint(a, setupRadii[2]) },
 					{ r: setupRadii[3], ...radialPoint(a, setupRadii[1]) }
@@ -77,7 +76,6 @@
 		radii = [
 			...setupRadii,
 			setupRadii[1] + setupRadii[3],
-			setupRadii[4] + setupRadii[2],
 			hexintersectRadius,
 			trilineIntersectRadius
 		].sort((a, b) => b - a),
@@ -85,21 +83,57 @@
 			radialPoint(anglesWithPhi[6], radii[1]),
 			radialPoint(anglesWithPhi[2], radii[1])
 		],
-		pentaLine: Line = [
-			radialPoint(anglesWithPhi[4], radii[1]),
-			radialPoint(anglesWithPhi[1], radii[5])
+		largeHexLine: Line = [
+			radialPoint(anglesWithPhi[3], radii[1]),
+			radialPoint(anglesWithPhi[8], radii[1])
 		],
-		hexPentaIntersection = intersection(smallHexline, pentaLine),
-		hexaPentaRadius = Math.sqrt(hexPentaIntersection.x ** 2 + hexPentaIntersection.y ** 2);
-	radii.push(hexaPentaRadius);
+		hexesIntersection = intersection(smallHexline, largeHexLine),
+		hexesIntersectionRadius = Math.sqrt(hexesIntersection.x ** 2 + hexesIntersection.y ** 2),
+		largeHexIntersection = intersection(largeHexLine, [
+			{ x: 0, y: 0 },
+			radialPoint(anglesWithPhi[6], radii[0])
+		]),
+		largeHexIntersectionRadius = Math.sqrt(
+			largeHexIntersection.x ** 2 + largeHexIntersection.y ** 2
+		),
+		line42: Line = [
+			radialPoint(anglesWithPhi[4], radii[2]),
+			radialPoint(anglesWithPhi[2], radii[1])
+		],
+		line42Intersection = intersection(line42, largeHexLine),
+		line42IntersectionRadius = Math.sqrt(line42Intersection.x ** 2 + line42Intersection.y ** 2),
+		line20: Line = [
+			radialPoint(anglesWithPhi[2], radii[1]),
+			radialPoint(anglesWithPhi[0], radii[1])
+		],
+		line20Intersection = intersection(line20, [
+			{ x: 0, y: 0 },
+			radialPoint(anglesWithPhi[1], radii[3])
+		]),
+		line20IntersectionRadius = Math.sqrt(line20Intersection.x ** 2 + line20Intersection.y ** 2);
+
+	radii.push(hexesIntersectionRadius);
+	radii.push(largeHexIntersectionRadius);
+	radii.push(line42IntersectionRadius);
+	radii.push(line20IntersectionRadius);
 	radii.sort((a, b) => b - a);
+
 	const hexPentAngle = Math.abs(
-			Math.atan2(hexPentaIntersection.y, hexPentaIntersection.x) * (180 / Math.PI)
+			Math.atan2(hexesIntersection.y, hexesIntersection.x) * (180 / Math.PI)
 		),
 		hexPentAngles = [...Array(6).keys()]
 			.map((n) => [(60 * n + hexPentAngle) % 360, (60 * n - hexPentAngle + 180) % 360])
 			.flat(),
-		angles = [...anglesWithPhi, ...hexPentAngles].sort((a, b) => b - a);
+		line42IntersectionAngle = Math.abs(
+			Math.atan2(line42Intersection.y, line42Intersection.x) * (180 / Math.PI)
+		),
+		line42Angles = [...Array(6).keys()]
+			.map((n) => [
+				(60 * n + line42IntersectionAngle) % 360,
+				(60 * n - line42IntersectionAngle + 180) % 360
+			])
+			.flat(),
+		angles = [...anglesWithPhi, ...hexPentAngles, ...line42Angles].sort((a, b) => b - a);
 
 	const parse = pathFromDSL(angles, radii);
 </script>
@@ -110,93 +144,135 @@
 			:root {
 				--radiiGuideColor: oklch(0.66 0.37 150);
 			}
-			circle,
-			path {
+			#guide circle,
+			#guide path {
 				fill: none;
-			}
-			#radii circle,
-			#radii path {
 				stroke: var(--radiiGuideColor);
 			}
-			#radii text {
+			#guide text {
 				fill: var(--radiiGuideColor);
 			}
 			#circles {
 				stroke: oklch(0.5 0.37 120);
 			}
-			#bigPenta path {
-				stroke: oklch(1 0.37 60);
+			.bigPenta path,
+			.medPenta path,
+			.tinyPenta path {
+				stroke: oklch(0.1 0.37 60);
 				stroke-width: 3;
-				fill: oklch(0.5 0.37 60 / 0.25);
+				fill: oklch(0.5 0.37 60);
+			}
+			.edges path {
+				fill: oklch(1 0.37 60);
+			}
+			#guide {
+				display: none;
+			}
+			.third {
+				/* display: none; */
 			}
 		</style>
 	</defs>
-	<Background {size} fill="black" />
-	<!-- <path d={`M${smallHexline[0].x} ${smallHexline[0].y}H${smallHexline[1].x}`} stroke="white" />
-	<path
-		d={`M${pentaLine[0].x} ${pentaLine[0].y}L${pentaLine[1].x} ${pentaLine[1].y}`}
-		stroke="white"
-	/> -->
-	<g id="radii">
-		{#each radii as r, i}
-			<circle {r} />
+	<Background {size} fill="oklch(0.075 0.2 240)" />
+	<g id="guide">
+		<path d={`M${smallHexline[0].x} ${smallHexline[0].y}H${smallHexline[1].x}`} stroke="white" />
+		<path d={`M${triline[0].x} ${triline[0].y}L${triline[1].x} ${triline[1].y}`} stroke="white" />
+		<path
+			d={`M${largeHexLine[0].x} ${largeHexLine[0].y}L${largeHexLine[1].x} ${largeHexLine[1].y}`}
+			stroke="white"
+		/>
+		<path d={`M${line42[0].x} ${line42[0].y}L${line42[1].x} ${line42[1].y}`} stroke="white" />
+		<circle cx={largeHexIntersection.x} cy={largeHexIntersection.y} r={10} stroke="white" />
+		<circle cx={line42Intersection.x} cy={line42Intersection.y} r={10} stroke="white" />
+		<g id="radii">
+			{#each radii as r, i}
+				<circle {r} />
+				<text
+					font-size={size / 75}
+					x={-size / 2 + size / 75}
+					y={-r}
+					alignment-baseline="middle"
+					text-anchor="middle">{i}</text
+				>
+				<path d={`M${-size / 2 + (size / 75) * 2} ${-r}H0`} />
+			{/each}
+		</g>
+		<g id="circles">
+			{#each circles as c, i}
+				<circle r={c.r} cx={c.x} cy={c.y} />
+			{/each}
+		</g>
+		<path id="hexagon" d={polygonPath(6, setupRadii[0])} stroke={`oklch(1 0.37 270)`} />
+		{#each angles as a, i}
+			<path d={`M0 0L${radialPointString(a, setupRadii[0])}`} stroke="white" />
 			<text
-				font-size={size / 75}
-				x={-size / 2 + size / 75}
-				y={-r}
-				alignment-baseline="middle"
-				text-anchor="middle">{i}</text
+				fill="white"
+				x={radialPoint(a, setupRadii[0] + size / 60).x}
+				y={radialPoint(a, setupRadii[0] + size / 60).y}
+				text-anchor="middle"
+				alignment-baseline="middle">{i}</text
 			>
-			<path d={`M${-size / 2 + (size / 75) * 2} ${-r}H0`} />
 		{/each}
 	</g>
-	<g id="circles">
-		{#each circles as c, i}
-			<circle r={c.r} cx={c.x} cy={c.y} />
+	<g id="background">
+		{#each angles as a, i}
+			<path
+				d={`M${radialPointString(a, radii[3])}L${radialPointString(a, (size / 2) * Math.sqrt(2))}`}
+				stroke="oklch(0.5 0.37 90)"
+				stroke-width={9}
+			/>
+			<path
+				d={`M${radialPointString(a, radii[3])}L${radialPointString(a, (size / 2) * Math.sqrt(2))}`}
+				stroke="oklch(1 0.37 90)"
+				stroke-width={3}
+			/>
 		{/each}
 	</g>
-	<path id="hexagon" d={polygonPath(6, setupRadii[0])} stroke={`oklch(1 0.37 270)`} />
-	{#each angles as a, i}
-		<path d={`M0 0L${radialPointString(a, setupRadii[0])}`} stroke="white" />
-		<text
-			fill="white"
-			x={radialPoint(a, setupRadii[0] + size / 60).x}
-			y={radialPoint(a, setupRadii[0] + size / 60).y}
-			text-anchor="middle"
-			alignment-baseline="middle">{i}</text
-		>
-	{/each}
-	{#each anglesArray(3, 0) as a}
-		<g id="bigPenta" transform={`rotate(${a})`}>
-			<!-- <path
-				class="outer"
-				d={parse('M4 10L4 7L7 4L4 5L4 1L2 4L0 1L0 5L27 4L0 7Z')}
-				stroke={`oklch(1 0.37 60)`}
-			/> -->
-			<g class="edges">
-				<path d={parse('M7 10L4 7L7 4Z')} />
-				<path d={parse('M7 4L4 5 L4 1Z')} />
-				<path d={parse('M4 1L2 4L0 1Z')} />
-				<path d={parse('M0 1L0 5L27 4Z')} />
-				<path d={parse('M27 4L0 7L27 10Z')} />
+	<g id="whole">
+		{#each anglesArray(3, 0) as a}
+			<g class="third" transform={`rotate(${a})`}>
+				<g class="bigPenta">
+					<g class="edges">
+						<path d={parse('M10 12L6 9L10 6Z')} />
+						<path d={parse('M10 6L6 7L6 1Z')} />
+						<path d={parse('M6 1L3 6L0 1Z')} />
+						<path d={parse('M0 1L0 7L38 6Z')} />
+						<path d={parse('M38 6L0 9L38 12Z')} />
+					</g>
+					<g class="starInner">
+						<path d={parse('M3 7L3 12L6 9Z')} />
+						<path d={parse('M3 7L6 9L10 6Z')} />
+						<path d={parse('M3 7L10 6L6 7Z')} />
+						<path d={parse('M3 7L6 7L6 1Z')} />
+						<path d={parse('M3 7L6 1L3 6Z')} />
+						<path d={parse('M3 7L3 6L0 1Z')} />
+						<path d={parse('M3 7L0 1L0 7Z')} />
+						<path d={parse('M3 7L0 7L38 6Z')} />
+						<path d={parse('M3 7L38 6L0 9Z')} />
+						<path d={parse('M3 7L0 9L0 12Z')} />
+					</g>
+				</g>
+				<g class="medPenta">
+					<g class="edges">
+						<path d={parse('M10 6L9 5L6 1Z')} />
+						<path d={parse('M8 3L7 1L6 1Z')} />
+						<path d={parse('M38 6L39 5L0 1Z')} />
+						<path d={parse('M0 1L41 1L40 3Z')} />
+					</g>
+					<g class="starInner">
+						<path d={parse('M10 2L10 6L9 5Z')} />
+						<path d={parse('M10 2L9 5L6 1Z')} />
+						<path d={parse('M10 2L7 1L8 3Z')} />
+						<path d={parse('M38 2L38 6L39 5Z')} />
+						<path d={parse('M38 2L39 5L0 1Z')} />
+						<path d={parse('M38 2L41 1L40 3Z')} />
+					</g>
+				</g>
+				<g class="tinyPenta">
+					<path d={parse('M6 1L3 2L3 4Z')} />
+					<path d={parse('M0 1L3 2L3 4Z')} />
+				</g>
 			</g>
-			<g class="starInner">
-				<path d={parse('M2 5L2 10L4 7Z')} />
-				<path d={parse('M2 5L7 4L4 7Z')} />
-				<path d={parse('M2 5L7 4L4 5Z')} />
-				<path d={parse('M2 5L4 5L4 1Z')} />
-				<path d={parse('M2 5L4 1L2 4Z')} />
-				<path d={parse('M2 5L2 4L0 1Z')} />
-				<path d={parse('M2 5L0 1L0 5Z')} />
-				<path d={parse('M2 5L0 5L27 4Z')} />
-				<path d={parse('M2 5L27 4L0 7Z')} />
-				<path d={parse('M2 5L0 7L0 10Z')} />
-			</g>
-		</g>
-		<g class="medPenta">
-			<g class="starInner">
-				<path d={parse('M4 3L4 1L')} />
-			</g>
-		</g>
-	{/each}
+		{/each}
+	</g>
 </svg>
