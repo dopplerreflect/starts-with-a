b4ee4d0beed0c8ff957ae775cebff864b46d609a
David Rose <doppler@gmail.com>
2023-04-29 07:37:51 -0700

    add 345

---
+++ src/routes/345/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:options namespace="svg" />
+
+<script lang="ts">
+	import { anglesArray, arrayMap, viewBox } from '$lib/geometry';
+
+	const size = 2 ** 8;
+	const r = 13;
+</script>
+
+<svg viewBox={viewBox(size)} transform="rotate(90)">
+	<defs>
+		<style>
+			circle,
+			path {
+				fill: none;
+				stroke: white;
+				stroke-linejoin: round;
+			}
+			circle {
+				/* display: none; */
+			}
+		</style>
+	</defs>
+	<g id="345">
+		<circle {r} />
+		<circle r={r * 2} />
+		<circle r={r * 3} />
+		<circle r={r * 4} />
+		<path d={`M${-r * 3} 0H${r * 3}M0 ${-r * 4}V${r * 4}`} />
+		<circle r={r * 5} cx={r * 3} />
+		<circle r={r * 5} cy={r * 4} />
+		<circle r={r * 5} cx={-r * 3} />
+		<circle r={r * 5} cy={-r * 4} />
+	</g>
+	<!-- <use href="#345" transform="rotate(90)" /> -->
+</svg>
