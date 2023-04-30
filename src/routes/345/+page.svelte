f59bb4c571b46602826e4b2c00ecdea4d4d63558
David Rose <doppler@gmail.com>
2023-04-30 15:11:18 -0700

    tweek 345

--- src/routes/345/+page.svelte
+++ src/routes/345/+page.svelte
@@ -1,13 +1,14 @@
 <svelte:options namespace="svg" />
 
 <script lang="ts">
-	import { anglesArray, arrayMap, viewBox } from '$lib/geometry';
+	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
+	import { viewBox } from '$lib/geometry';
 
 	const size = 2 ** 8;
-	const r = 13;
+	const r = size / 19;
 </script>
 
-<svg viewBox={viewBox(size)} transform="rotate(90)">
+<DopplerSvg id="345" viewBox={viewBox(size)}>
 	<defs>
 		<style>
 			circle,
@@ -26,11 +27,12 @@
 		<circle r={r * 2} />
 		<circle r={r * 3} />
 		<circle r={r * 4} />
+		<circle r={r * 5} />
 		<path d={`M${-r * 3} 0H${r * 3}M0 ${-r * 4}V${r * 4}`} />
+		<path d={`M${-r * 3} 0L0 ${-r * 4}L${r * 3} 0L0 ${r * 4}Z`} />
 		<circle r={r * 5} cx={r * 3} />
 		<circle r={r * 5} cy={r * 4} />
 		<circle r={r * 5} cx={-r * 3} />
 		<circle r={r * 5} cy={-r * 4} />
 	</g>
-	<!-- <use href="#345" transform="rotate(90)" /> -->
-</svg>
+</DopplerSvg>
