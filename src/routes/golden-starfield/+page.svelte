9f1ba69bfee6350fa3a5f27a86bc094c0b090627
David Rose <doppler@gmail.com>
2023-04-30 10:13:46 -0700

    golden-starfield: refactor and add hue spiral

--- src/routes/golden-starfield/+page.svelte
+++ src/routes/golden-starfield/+page.svelte
@@ -2,33 +2,31 @@
 
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
-	import {
-		anglesArray,
-		arrayMap,
-		pentagram,
-		phi,
-		radialPoint,
-		radialPointString,
-		viewBox
-	} from '$lib/geometry';
-	import { pathFromDSL } from '$lib/path-parser';
+	import { anglesArray, arrayMap, pentagram, phi, radialPoint, viewBox } from '$lib/geometry';
 	import { useSaveFile } from '$lib/save-svg';
+	import { useZoomableViewbox } from '$lib/use-zoomable-viewbox';
 	import { onMount } from 'svelte';
 
 	let svg: SVGSVGElement;
 	onMount(() => {
 		const unmountSaveFile = useSaveFile(svg, { timestamp: true });
+		const unmountZoom = useZoomableViewbox(svg);
 		return () => {
 			unmountSaveFile();
+			unmountZoom();
 		};
 	});
 	const size = 2 ** 10;
 	const r = size / 3;
-	const angles = arrayMap(10, (n) => 36 * n - 18);
-	const angles10 = anglesArray(10, 0);
-	const radii = arrayMap(6, (n) => r * phi ** n);
+	const radii = arrayMap(12, (n) => r * phi ** n);
 
-	const parse = pathFromDSL(angles, radii);
+	const pentagramPaths = anglesArray(10, 0)
+		.map((a) =>
+			[0, 1, 2, 3, 4, 5, 6, 7].map((i) =>
+				pentagram(radii[i + 2], { center: radialPoint(a, radii[i]), rotate: a + 18 })
+			)
+		)
+		.flat();
 </script>
 
 <svg bind:this={svg} id="golden-starfield" viewBox={viewBox(size)}>
@@ -37,33 +35,21 @@
 			path,
 			circle {
 				stroke: black;
-				stroke-width: 2;
 			}
 			path.f {
-				fill: oklch(1 0.37 60 / 0.5);
+				fill: oklch(0.5 0.37 240 / 0.5);
 			}
 			.lotsapentagrams path {
-				fill: oklch(1 0.37 60 / 1);
+				/* fill: oklch(1 0.37 60 / 1); */
 				/* fill: none; */
-				/* fill-rule: evenodd; */
+				fill-rule: evenodd;
 			}
 		</style>
 	</defs>
 	<Background {size} fill="white" />
 	<g class="lotsapentagrams">
-		{#each angles10 as a}
-			<path d={pentagram(radii[2], { center: radialPoint(a, radii[0]), rotate: a + 18 })} />
-			<path d={pentagram(radii[3], { center: radialPoint(a, radii[1]), rotate: a + 18 })} />
-			<path d={pentagram(radii[4], { center: radialPoint(a, radii[2]), rotate: a + 18 })} />
-			<path d={pentagram(radii[5], { center: radialPoint(a, radii[3]), rotate: a + 18 })} />
-			<path d={pentagram(radii[5] * phi, { center: radialPoint(a, radii[4]), rotate: a + 18 })} />
-			<path
-				d={pentagram(radii[5] * phi ** 2, { center: radialPoint(a, radii[5]), rotate: a + 18 })}
-			/>
+		{#each pentagramPaths as path, i}
+			<path d={path} fill={`oklch(0.88 0.37 ${30 + (i % 9) * 10})`} />
 		{/each}
 	</g>
-	{#each anglesArray(20) as a, i}
-		<path d={`M${radialPointString(a, radii[5])}L${radialPointString(a, size)}`} />
-	{/each}
-	<path class="f" d={parse('M0 0L4 0L8 0L 2 0L6 0Z')} />
 </svg>
