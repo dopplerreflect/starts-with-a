5ee1ee4086ad14d2bb057a91b77966c19543cc69
David Rose <doppler@gmail.com>
2023-05-01 09:20:07 -0700

    DopplerSVG: pass zoom etc

--- src/lib/components/DopplerSVG.svelte
+++ src/lib/components/DopplerSVG.svelte
@@ -8,11 +8,15 @@
 	export let viewBox: string;
 	export let id: string;
 
+	export let zoom: number = 0;
+	export let xPan: number = 0;
+	export let yPan: number = 0;
+
 	let svg: SVGSVGElement;
 
 	onMount(() => {
 		const unmountSaveFile = useSaveFile(svg, { timestamp: true });
-		const unmountZoom = useZoomableViewbox(svg);
+		const unmountZoom = useZoomableViewbox(svg, zoom, xPan, yPan);
 		return () => {
 			unmountSaveFile();
 			unmountZoom();
