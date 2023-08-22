f4efb698a30c9f8dcc34e28fc14ac3dabe944654
David Rose <doppler@gmail.com>
2023-08-22 11:49:40 -0600

    add oklch

---
+++ src/routes/oklch/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
+	import { anglesArray, viewBox } from '$lib/geometry';
+
+	const size = 2 ** 10;
+	const angles = anglesArray(360 / 15, 0);
+	const rectSize = size / angles.length;
+</script>
+
+<DopplerSvg viewBox={viewBox(size)} id="oklch">
+	{#each angles as a, i}
+		<path
+			d={`M${-size / 2} ${-size / 2 + i * rectSize}h${rectSize}v${rectSize}h${-rectSize}Z`}
+			fill={`oklch(75% 0.3 ${a})`}
+		/>
+		<text
+			text-anchor="middle"
+			alignment-baseline="middle"
+			fill="white"
+			x={-size / 2 + rectSize / 2}
+			y={-size / 2 + i * rectSize + rectSize / 2}>{a}</text
+		>
+	{/each}
+</DopplerSvg>
