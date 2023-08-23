6a3910bb484428694a2d780b6ddf5e62d3d9d842
David Rose <doppler@gmail.com>
2023-08-22 22:41:03 -0600

    add new4

--- src/routes/new4/+page.svelte
+++ src/routes/new4/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
-	import { Phi, anglesArray, pointToString as ps, radialPoint, viewBox } from '$lib/geometry';
+	import {
+		Phi,
+		anglesArray,
+		pointToString as ps,
+		radialPoint,
+		starPath,
+		viewBox
+	} from '$lib/geometry';
 
-	const size = 2 ** 8;
+	const size = 2 ** 9;
 	const angles = anglesArray(6);
 	const r = size / 11;
 	const radii = [r, r * Phi, r * Phi ** 2];
 	const vr = Math.sqrt(radii[2] ** 2 - (radii[2] / 2) ** 2) * 2; // radius to vesica formed by circle 3
-	const p = anglesArray(6, 0).map((a) => radialPoint(a, vr)); // points for outer path
-	const rs = `${radii[2]} ${radii[2]} 0 0 1`;
 </script>
 
 <DopplerSvg id="new4" viewBox={viewBox(size)}>
@@ -16,14 +21,27 @@
 		<style>
 			circle,
 			path {
-				stroke: oklch(1 0 0);
+				stroke: oklch(90% 0.37 90);
+				stroke-width: 1;
 				fill: none;
 			}
 			circle.dim {
-				stroke: oklch(0.25 0 0);
+				stroke: oklch(0.33 0.2 300);
+				stroke-width: 1;
+			}
+			circle.mask {
+				fill: black;
+			}
+			path.blur {
+				stroke-width: 3;
+				filter: url(#blur);
 			}
 		</style>
+		<filter id="blur">
+			<feGaussianBlur in="SourceGraphic" stdDeviation="3" />
+		</filter>
 	</defs>
+	<circle class="dim" r={radii[1]} />
 	{#each angles as a}
 		<circle
 			class="dim"
@@ -38,13 +56,8 @@
 			cy={radialPoint(a, radii[2]).y}
 		/>
 	{/each}
-	{#each angles as a}
-		<circle {r} cx={radialPoint(a, radii[2]).x} cy={radialPoint(a, radii[2]).y} />
-	{/each}
-	<circle {r} />
-	<path
-		d={`M${ps(p[0])}A${rs} ${ps(p[1])}A${rs} ${ps(p[2])}A${rs} ${ps(p[3])}A${rs} ${ps(
-			p[4]
-		)}A${rs} ${ps(p[5])}A${rs} ${ps(p[0])}Z`}
-	/>
+	<circle class="dim" r={radii[2]} />
+	<circle class="dim" r={radii[2] + radii[1]} />
+	<path class="blur" d={starPath(radii[2] + radii[1])} stroke-linejoin="bevel" />
+	<path d={starPath(radii[2] + radii[1])} stroke-linejoin="bevel" />
 </DopplerSvg>
