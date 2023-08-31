c2503b9d1a5cfb21eb5745364910e696e3b02529
David Rose <doppler@gmail.com>
2023-08-31 13:54:01 -0600

    refactor phi-yantra

--- src/routes/phi-yantra/+page.svelte
+++ src/routes/phi-yantra/+page.svelte
@@ -1,3 +1,5 @@
+<svelte:options namespace="svg" />
+
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
@@ -13,50 +15,26 @@
 		{ r: r * SQRT3, x: r, y: 0 },
 		{ r: r * Math.sqrt(2), x: 0, y: 0 }
 	];
-	// const squareLengths = [r * Phi, r, r * phi, r * phi ** 2];
-	// const squares = squareLengths.map((l) => [
-	// 	{ x: -l, y: l },
-	// 	{ x: l, y: -l },
-	// 	{ x: l, y: l },
-	// 	{ x: -l, y: l }
-	// ]);
-	const s1: Point[] = [
-		{ x: -r * Phi, y: -r * Phi },
-		{ x: r * Phi, y: -r * Phi },
-		{ x: r * Phi, y: r * Phi },
-		{ x: -r * Phi, y: r * Phi }
-	];
-	const s2: Point[] = [
-		{ x: -r, y: -r },
-		{ x: r, y: -r },
-		{ x: r, y: r },
-		{ x: -r, y: r }
-	];
-	const s3: Point[] = [
-		{ x: -r * phi, y: -r * phi },
-		{ x: r * phi, y: -r * phi },
-		{ x: r * phi, y: r * phi },
-		{ x: -r * phi, y: r * phi }
-	];
-	const s4: Point[] = [
-		{ x: -r * phi ** 2, y: -r * phi ** 2 },
-		{ x: r * phi ** 2, y: -r * phi ** 2 },
-		{ x: r * phi ** 2, y: r * phi ** 2 },
-		{ x: -r * phi ** 2, y: r * phi ** 2 }
-	];
+	const squareLengths = [r * Phi, r, r * phi, r * phi ** 2];
+	const squares = squareLengths.map((l) => [
+		{ x: -l, y: -l },
+		{ x: l, y: -l },
+		{ x: l, y: l },
+		{ x: -l, y: l }
+	]);
 	const yp: Point[] = [
-		{ x: s1[0].x, y: s1[0].y },
-		{ x: s1[1].x, y: s4[2].y },
-		{ x: s1[3].x, y: s4[2].y },
-		{ x: s1[1].x, y: s1[1].y },
-		{ x: s4[3].x, y: s1[2].y },
-		{ x: s4[3].x, y: s1[0].y },
-		{ x: s1[2].x, y: s1[2].y },
-		{ x: s1[3].x, y: s4[0].y },
-		{ x: s1[1].x, y: s4[0].y },
-		{ x: s1[3].x, y: s1[3].y },
-		{ x: s4[2].x, y: s1[0].y },
-		{ x: s4[2].x, y: s1[3].y }
+		{ x: squares[0][0].x, y: squares[0][0].y },
+		{ x: squares[0][1].x, y: squares[3][2].y },
+		{ x: squares[0][3].x, y: squares[3][2].y },
+		{ x: squares[0][1].x, y: squares[0][1].y },
+		{ x: squares[3][3].x, y: squares[0][2].y },
+		{ x: squares[3][3].x, y: squares[0][0].y },
+		{ x: squares[0][2].x, y: squares[0][2].y },
+		{ x: squares[0][3].x, y: squares[3][0].y },
+		{ x: squares[0][1].x, y: squares[3][0].y },
+		{ x: squares[0][3].x, y: squares[0][3].y },
+		{ x: squares[3][2].x, y: squares[0][0].y },
+		{ x: squares[3][2].x, y: squares[0][3].y }
 	];
 </script>
 
@@ -76,7 +54,7 @@
 				stroke-width: 2;
 				stroke-linejoin: bevel;
 				stroke: oklch(1 0.37 90 / 1);
-				fill: oklch(1 0.37 75 / 0.5);
+				fill: url(#rGradient);
 				fill-rule: evenodd;
 			}
 		</style>
@@ -88,27 +66,25 @@
 				<feMergeNode in="SourceGraphic" />
 			</feMerge>
 		</filter>
+		<radialGradient id="rGradient">
+			<stop offset="0%" stop-color="oklch(1 0.37 90 / 0.5)" />
+			<stop offset="100%" stop-color="oklch(0.5 0.37 90 / 0.5)" />
+		</radialGradient>
 	</defs>
 	<Background {size} fill="oklch(0.1 0.175 300)" />
-
 	<line x1={-size / 2} x2={size / 2} />
-
 	{#each circles as c, i}
 		<circle r={c.r} cx={c.x} cy={c.y} />
 	{/each}
-	<line x1={s1[0].x} y1={s1[0].y} x2={s1[2].x} y2={s1[2].y} />
-	<line x1={s1[1].x} y1={s1[1].y} x2={s1[3].x} y2={s1[3].y} />
-
-	<line x1={s4[0].x} y1={s1[0].y} x2={s4[0].x} y2={s1[3].y} />
-	<line x1={s4[1].x} y1={s1[0].y} x2={s4[1].x} y2={s1[3].y} />
-
-	<line x1={s1[0].x} y1={s4[0].y} x2={s1[1].x} y2={s4[0].y} />
-	<line x1={s1[0].x} y1={s4[2].y} x2={s1[1].x} y2={s4[2].y} />
-
-	<path class="s" d={`M${s1.map((p) => pointToString(p)).join(' ')}Z`} />
-	<path class="s" d={`M${s2.map((p) => pointToString(p)).join(' ')}Z`} />
-	<path class="s" d={`M${s3.map((p) => pointToString(p)).join(' ')}Z`} />
-	<path class="s" d={`M${s4.map((p) => pointToString(p)).join(' ')}Z`} />
-
+	<line x1={squares[0][0].x} y1={squares[0][0].y} x2={squares[0][2].x} y2={squares[0][2].y} />
+	<line x1={squares[0][1].x} y1={squares[0][1].y} x2={squares[0][3].x} y2={squares[0][3].y} />
+	<line x1={squares[3][0].x} y1={squares[0][0].y} x2={squares[3][0].x} y2={squares[0][3].y} />
+	<line x1={squares[3][1].x} y1={squares[0][0].y} x2={squares[3][1].x} y2={squares[0][3].y} />
+	<line x1={squares[0][0].x} y1={squares[3][0].y} x2={squares[0][1].x} y2={squares[3][0].y} />
+	<line x1={squares[0][0].x} y1={squares[3][2].y} x2={squares[0][1].x} y2={squares[3][2].y} />
+	<path class="s" d={`M${squares[0].map((p) => pointToString(p)).join(' ')}Z`} />
+	<path class="s" d={`M${squares[1].map((p) => pointToString(p)).join(' ')}Z`} />
+	<path class="s" d={`M${squares[2].map((p) => pointToString(p)).join(' ')}Z`} />
+	<path class="s" d={`M${squares[3].map((p) => pointToString(p)).join(' ')}Z`} />
 	<path class="y" d={`M${yp.map((p) => pointToString(p)).join(' ')}Z`} filter="url(#blur)" />
 </DopplerSvg>
