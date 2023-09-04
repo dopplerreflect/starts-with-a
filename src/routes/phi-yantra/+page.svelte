9e4692acc2bcb413ac95cb7903e9e9bf89d3fa46
David Rose <doppler@gmail.com>
2023-09-03 20:21:40 -0600

    update phi-yantra

--- src/routes/phi-yantra/+page.svelte
+++ src/routes/phi-yantra/+page.svelte
@@ -3,7 +3,7 @@
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
 	import DopplerSvg from '$lib/components/DopplerSVG.svelte';
-	import { Phi, SQRT3, anglesArray, phi, pointToString, radialPoint, viewBox } from '$lib/geometry';
+	import { Phi, SQRT3, phi, pointToString, viewBox } from '$lib/geometry';
 
 	const size = 2 ** 10;
 	const r = size / Math.sqrt(3) / 2;
@@ -44,23 +44,28 @@
 			path:not(.Background),
 			circle,
 			line {
-				fill: none;
-				stroke: grey;
+				stroke: white;
+				/* filter: url(#blur); */
+			}
+			circle {
+				fill: oklch(0.25 0.37 300 / 0.24);
 			}
 			path.s {
 				stroke: white;
+				fill: oklch(0.25 0.37 300 / 0.24);
 			}
 			path.y {
 				stroke-width: 2;
 				stroke-linejoin: bevel;
 				stroke: oklch(1 0.37 90 / 1);
 				fill: url(#rGradient);
+				filter: url(#blur);
 				fill-rule: evenodd;
 			}
 		</style>
 		<filter id="blur">
-			<feMorphology in="SourceGraphic" operator="dilate" radius="1" result="dilate" />
-			<feGaussianBlur in="dilate" stdDeviation="10" result="blur" />
+			<feMorphology in="StrokePaint" operator="dilate" radius="1" result="dilate" />
+			<feGaussianBlur in="dilate" stdDeviation="3" result="blur" />
 			<feMerge>
 				<feMergeNode in="blur" />
 				<feMergeNode in="SourceGraphic" />
@@ -68,7 +73,7 @@
 		</filter>
 		<radialGradient id="rGradient">
 			<stop offset="0%" stop-color="oklch(1 0.37 90 / 0.5)" />
-			<stop offset="100%" stop-color="oklch(0.5 0.37 90 / 0.5)" />
+			<stop offset="100%" stop-color="oklch(0.5 0.37 30 / 0.125)" />
 		</radialGradient>
 	</defs>
 	<Background {size} fill="oklch(0.1 0.175 300)" />
@@ -86,5 +91,5 @@
 	<path class="s" d={`M${squares[1].map((p) => pointToString(p)).join(' ')}Z`} />
 	<path class="s" d={`M${squares[2].map((p) => pointToString(p)).join(' ')}Z`} />
 	<path class="s" d={`M${squares[3].map((p) => pointToString(p)).join(' ')}Z`} />
-	<path class="y" d={`M${yp.map((p) => pointToString(p)).join(' ')}Z`} filter="url(#blur)" />
+	<path class="y" d={`M${yp.map((p) => pointToString(p)).join(' ')}Z`} />
 </DopplerSvg>
