81253965063f89d30463e6754392e4f3c19e9d1d
David Rose <doppler@gmail.com>
2023-09-09 14:59:36 -0600

    DopplerSvg: use size, making viewBox optional

--- src/lib/components/DopplerSVG.svelte
+++ src/lib/components/DopplerSVG.svelte
@@ -1,13 +1,15 @@
 <svelte:options namespace="svg" />
 
 <script lang="ts">
+	import { viewBox as vb } from '$lib/geometry';
 	import { useSaveFile } from '$lib/save-svg';
 	import { useZoomableViewbox } from '$lib/use-zoomable-viewbox';
 	import { onMount } from 'svelte';
 
-	export let viewBox: string;
+	export let viewBox: string | undefined = '';
 	export let id: string;
 
+	export let size: number = 2 ** 10;
 	export let zoom: number = 0;
 	export let xPan: number = 0;
 	export let yPan: number = 0;
@@ -24,6 +26,6 @@
 	});
 </script>
 
-<svg bind:this={svg} {viewBox} {id}>
+<svg bind:this={svg} {id} viewBox={viewBox ? viewBox : vb(size)}>
 	<slot />
 </svg>
